fdc536bac81f5ba957b10c56ee0effca54987ddb
Ashley-tech <58460632+Ashley-tech@users.noreply.github.com>
2025-02-07 11:55:34 +0100

    Add files via upload

---
+++ exogateway/exogateway/src/test/java/com/example/exogateway/ExogatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExogatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
